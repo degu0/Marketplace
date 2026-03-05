c96fbff9953bad7090803eca5e6d2e89666058c3
degu0 <yatog2013@gmail.com>
2026-03-05 19:06:24 -0300

    feat: add DTO

---
+++ Backend/Backend/DTOs/ProductResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.DTOs
+{
+    public class ProductResponseDto
+    {
+        public string Product {  get; set; }
+        public List<ResponseProductVariantDto> Variants { get; set; }
+    }
+}
---
+++ Backend/Backend/DTOs/ProductVariantDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.DTOs
+{
+    public class ResponseProductVariantDto
+    {
+        public string Sku { get; set; }
+        public Dictionary<string, string> Attributes { get; set; } = new();
+    }
+}
