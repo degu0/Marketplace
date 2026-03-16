2d325eb8594d90a5223df9fb76cd9266262581b9
degu0 <yatog2013@gmail.com>
2026-03-16 13:55:18 -0300

    feat: Add user controller

---
+++ Backend/Backend/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Backend.DTOs.User;
+using Backend.Services.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatedUser(CreateUserDto dto)
+        {
+            var createdUser = await _service.CreatedUser(dto);
+            return CreatedAtAction(nameof(GetById), new { id = createdUser.Id }, createdUser);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var user = await _service.GetById(id);
+            if(user == null)
+                return NotFound();
+
+            return Ok(user);  
+        }
+    }
+}
